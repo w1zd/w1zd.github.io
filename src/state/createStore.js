06de9f6747608923edcac5d2f92f84e9340d0795
AGG <pan@agg.me>
2022-08-30 15:31:40 +0800

    fix: fix redux ssr bug

--- src/state/createStore.js
+++ src/state/createStore.js
@@ -1,5 +1,7 @@
 import { createStore as reduxCreateStore } from "redux"
 
+const isBrowser = typeof window !== "undefined"
+
 const reducer = (state, action) => {
   if (action.type === `SET_THEME`) {
     localStorage.setItem('theme', action.payload);
@@ -9,8 +11,7 @@ const reducer = (state, action) => {
   }
   return state
 }
-
-const initialState = { theme: localStorage.getItem('theme') || 'Light' }
+const initialState = { theme: isBrowser ? localStorage.getItem('theme') || 'Light' : 'Light' }
 
 const createStore = () => reduxCreateStore(reducer, initialState)
 export default createStore
