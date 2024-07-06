47a3a69040cde16d3c685241463ca58ed615b15b
w1zd <me@wizd.dev>
2024-07-07 01:45:52 +1200

    feat: moments

--- src/pages/moments.tsx
+++ src/pages/moments.tsx
@@ -1,14 +1,24 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { graphql, Link } from "gatsby"
 import Layout from "../components/layout"
+import images from "../../content/gallery.json"
+import mediumZoom from "medium-zoom"
 
 const Moments = ({ data }) => {
+  useEffect(() => {
+    mediumZoom(".gallery-item img", { background: "#292a2d" })
+  }, []);
   return (
-    <Layout title='About Me'>
+    <Layout title="About Me">
       <div className="container">
-        <div className="post-wrap categories">
-          <h1>🚧 🚧 🚧</h1>
-          <h2>Coming Soon ...  </h2>
+        <div className="post-wrap gallery">
+          {images.map(v => {
+            return (
+              <div className='gallery-item' key={v.sha}>
+                <img src={v.download_url} alt="" />
+              </div>
+            )
+          })}
         </div>
       </div>
     </Layout>
