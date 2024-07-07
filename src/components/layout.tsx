5ab50c1e97f3073e3fd77071820833ed686357c2
w1zd <me@wizd.dev>
2024-07-07 22:56:03 +1200

    feat: use github api get gallery

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,6 +4,7 @@ import Header from "./header"
 
 // import { rhythm, scale } from "../utils/typography"
 import "../../theme-chic/css/base.styl"
+import 'react-medium-image-zoom/dist/styles.css'
 import { ReactComponentLike } from "prop-types"
 import SEO from "./seo"
 
--- src/pages/moments.tsx
+++ src/pages/moments.tsx
@@ -1,21 +1,52 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { graphql, Link } from "gatsby"
 import Layout from "../components/layout"
-import images from "../../content/gallery.json"
-import mediumZoom from "medium-zoom"
+import Zoom from "react-medium-image-zoom"
+
+const loadGallery = async () => {
+  return await fetch(
+    "https://api.github.com/repos/w1zd/image-hosting/contents/gallery",
+    {
+      method: "get",
+      headers: {
+        Authorization:
+          "Bearer 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",
+      },
+    }
+  )
+}
 
 const Moments = ({ data }) => {
+  const [gallery, setGallery] = useState([])
   useEffect(() => {
-    mediumZoom(".gallery-item img", { background: "#292a2d" })
-  }, []);
+    (async () => {
+      try {
+        const res = await loadGallery()
+        setGallery(await res.json())
+      } catch (error) {}
+    })();
+  }, [])
+
   return (
     <Layout title="About Me">
       <div className="container">
         <div className="post-wrap gallery">
-          {images.map(v => {
+          {gallery.map(v => {
             return (
-              <div className='gallery-item' key={v.sha}>
-                <img src={v.download_url} alt="" />
+              <div className="gallery-item" key={v.sha}>
+                <Zoom
+                  classDialog="test-class"
+                  zoomImg={{
+                    src: `https://raw.githubusercontent.com/w1zd/image-hosting/main/gallery/${v.name}`,
+                    width: "100%",
+                    height: "auto",
+                  }}
+                >
+                  <img
+                    src={`https://raw.githubusercontent.com/w1zd/image-hosting/main/thumbnails/280/${v.name}`}
+                    alt=""
+                  />
+                </Zoom>
               </div>
             )
           })}
