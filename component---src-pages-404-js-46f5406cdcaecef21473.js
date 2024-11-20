625be6c2edde91761be8dc63487fa65b0e6a251c
w1zd <w1zd@users.noreply.github.com>
2024-11-20 14:16:09 +0000

    deployed via Gatsby Publish Action 🎩 for f506d261b59bbbc8a35cb34cbd40524cbae9a2e2

---
+++ component---src-pages-404-js-46f5406cdcaecef21473.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkw1zd_lair=self.webpackChunkw1zd_lair||[]).push([[5125],{3331:function(t,e,a){a.r(e);var l=a(6540),n=a(7947);e.default=t=>{let{data:e,location:a}=t;return l.createElement(n.A,{location:a,title:"404: Not Found"},l.createElement("h1",null,"Not Found"),l.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-46f5406cdcaecef21473.js.map
